00d4ff5cd4987fa2451cffbb2783ee2c9510a8e3
Antoine Paturel <antoine.paturel34@free.fr>
2024-01-12 14:15:46 +0100

    development

---
+++ src/pages/AuthPage/Signin.jsx
@@ -0,0 +1,59 @@
+import { useAtom } from 'jotai';
+import { useNavigate } from 'react-router-dom'
+
+import { userAtom } from '../../atoms/userAtom';
+import './Form.css';
+
+const Signin = () => {
+  const navigate = useNavigate();
+  const [, setUser] = useAtom(userAtom);
+
+  const handleSigninSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const userData = Object.fromEntries(formData.entries());
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/users/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erreur lors de la connection');
+      }
+
+      const data = await response.json();
+      setUser({
+        isLoggedIn: true,
+        username: data.username,
+        token: data.token
+      });
+      localStorage.setItem('userInfos', JSON.stringify({ username: data.username, token: data.token }));
+      navigate('/');
+
+    } catch (error) {
+      console.error('Erreur de connection:', error);
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <div className="form-card">
+        <form onSubmit={handleSigninSubmit}>
+          <div className="form-row">
+            <input type="text" placeholder="Pseudo" name="username" />
+            <input type="password" placeholder="Mot de passe" name="password" />
+          </div>
+          <div className="form-row">
+            <button type="submit">Se connecter</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+export default Signin
